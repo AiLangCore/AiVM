0f4a5404509486b1d995d0f7b6e473ea24c59b2e
Todd Henderson <todd@todd-henderson.me>
2026-02-12 00:06:48 -0600

    Bootstrap AiVM.Core with runtime host primitives

---
+++ AosProcessExitException.cs
@@ -0,0 +1,11 @@
+namespace AiVM.Core;
+
+public sealed class AosProcessExitException : Exception
+{
+    public AosProcessExitException(int code)
+    {
+        Code = code;
+    }
+
+    public int Code { get; }
+}
